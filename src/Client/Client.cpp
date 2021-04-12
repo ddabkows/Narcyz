4779da00698f0b3ac51fe7bcd599d27eb894a973
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 14:57:08 +0200

    Created a main file

---
+++ src/Client/Client.cpp
@@ -0,0 +1,16 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Client main cpp
+*/
+
+
+#include "../Game/GameBoard.hpp"
+#include "../Game/Dimensions.hpp"
+
+
+int main() {
+  GameBoard gameboard(800, 600);
+
+  return 0;
+}
