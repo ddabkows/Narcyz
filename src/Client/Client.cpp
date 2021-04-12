cfc053337b5be873ddd8cd6f74a07d55c545e42a
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 15:45:25 +0200

    Create main to manipulate Game

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -7,11 +7,39 @@
 
 #include "../Game/GameBoard.hpp"
 #include "../Game/Dimensions.hpp"
+#include "../Game/Game.hpp"
 #include <iostream>
 
 
 int main() {
-  GameBoard gameboard(800, 600);
-
+  Game game;
+  bool end_game = false;
+  while (!end_game) {
+    std::cout << "Board : " << std::endl;
+    std::cout << 0 << "--------" << game.getBoardSize().x << std::endl;
+    std::cout << "Player : " << std::endl;
+    std::cout << "---" << game.getPlayerPosition().x << "---" << std::endl;
+    std::cout << "Want to move ?" << std::endl;
+    char decision;
+    std::cin >> decision;
+    std::cout << "\n";
+    switch (decision) {
+      case 'a' : {
+        game.movePlayerHorizontal(-1);
+        break;
+      }
+      case 'd' : {
+        game.movePlayerHorizontal(1);
+        break;
+      }
+      case 's' : {
+        end_game = true;
+        break;
+      }
+      default : {
+        break;
+      }
+    }
+  }
   return 0;
 }
