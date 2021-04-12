51bfc34dc425a2bc6d1438dea9aa2607d688b449
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 17:47:21 +0200

    Added event results

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -9,24 +9,47 @@
 #include <SFML/Graphics.hpp>
 #include <SFML/Audio.hpp>
 #include <SFML/Window.hpp>
+#include <iostream>
 #include "../Game/GameBoard.hpp"
 #include "../Game/Dimensions.hpp"
 #include "../Game/Game.hpp"
 #include "../GUI/Master.hpp"
-#include <iostream>
+#include "../GUI/GameWindow.hpp"
 
 
 int main() {
   Game game;
   Master master_gui;
+  GameWindow game_win(game.getPlayerSize().x, game.getPlayerSize().y, game.getPlayerPosition().x, game.getPlayerPosition().y);
   bool keepProgramOpen = true;
+  int game_win_output;
   while (keepProgramOpen) {
     sf::Event event;
 
-    while(master_gui.pollEvent(event)) {}
+    while(master_gui.pollEvent(event)) {
+      game_win_output = game_win.processEvent(event);
+    }
+
+    switch (game_win_output) {
+      case 0 : {
+        master_gui.closeWindow();
+        break;
+      }
+      case 1 : {
+        game.movePlayerHorizontal(1);
+        break;
+      }
+      case 2 : {
+        game.movePlayerHorizontal(-1);
+        break;
+      }
+      default : {
+        break;
+      }
+    }
 
     master_gui.clearWindow();
-    master_gui.drawWindow();
+    game_win.drawWindow(master_gui);
     master_gui.displayWindow();
 
     if (!master_gui.getOpen()) {
