3b3415f0cf5a4ee6c19d6f165841cc9c1fbd36a8
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 16:23:26 +0200

    New master, this time class

--- src/GUI/Master.hpp
+++ src/GUI/Master.hpp
@@ -15,7 +15,36 @@
 #define _MASTER_H_
 
 
-struct Master {
-  std::shared_ptr<sf::RenderWindow> window;
+class Master {
+  private:
+    // Trait
+    using myClass = Master;
+
+    const int _GAMEBOARD_SIZE_X = 800;
+    const int _GAMEBOARD_SIZE_Y = 600;
+
+    std::shared_ptr<sf::RenderWindow> window;
+  public:
+    // Constructor
+    Master() : window() {};
+
+    // Copy
+    Master(const myClass&) = delete;
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    Master(myClass&&) = delete;
+    myClass& operator=(myClass&&) = delete;
+
+    // Methods
+    void setupWindow();
+
+    // Setters
+
+    // Getters
+
+    // Destructor
+    ~Master() = default;
+
 };
 #endif
