00a5ff0463f2ae489f5becbf1edac5bed39d49e1
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 16:50:14 +0200

    First Client use

--- src/Client/Client.cpp
+++ src/Client/Client.cpp
@@ -5,40 +5,32 @@
 */
 
 
+#include <memory>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+#include <SFML/Window.hpp>
 #include "../Game/GameBoard.hpp"
 #include "../Game/Dimensions.hpp"
 #include "../Game/Game.hpp"
+#include "../GUI/Master.hpp"
 #include <iostream>
 
 
 int main() {
   Game game;
-  bool end_game = false;
-  while (!end_game) {
-    std::cout << "Board : " << std::endl;
-    std::cout << 0 << "--------" << game.getBoardSize().x << std::endl;
-    std::cout << "Player : " << std::endl;
-    std::cout << "---" << game.getPlayerPosition().x << "---" << std::endl;
-    std::cout << "Want to move ?" << std::endl;
-    char decision;
-    std::cin >> decision;
-    std::cout << "\n";
-    switch (decision) {
-      case 'a' : {
-        game.movePlayerHorizontal(-1);
-        break;
-      }
-      case 'd' : {
-        game.movePlayerHorizontal(1);
-        break;
-      }
-      case 's' : {
-        end_game = true;
-        break;
-      }
-      default : {
-        break;
-      }
+  Master master_gui;
+  bool keepProgramOpen = true;
+  while (keepProgramOpen) {
+    sf::Event event;
+
+    while(master_gui.pollEvent(event)) {}
+
+    master_gui.clearWindow();
+    master_gui.drawWindow();
+    master_gui.displayWindow();
+
+    if (!master_gui.getOpen()) {
+      keepProgramOpen = false;
     }
   }
   return 0;
