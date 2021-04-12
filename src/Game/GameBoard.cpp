e732a526b444c49620f3579f03b963521fa824fe
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 15:16:56 +0200

    Insignificant changes

--- src/Game/GameBoard.cpp
+++ src/Game/GameBoard.cpp
@@ -10,16 +10,10 @@
 
 // Setters
 void GameBoard::setSize(int size_x, int size_y) {
-  /*
-    Method made to set the size of the GameInsideBounds
-  */
-
   _inside_limits.x = size_x;
   _inside_limits.y = size_y;
 }
 
 
 // Getters
-const Dimensions& GameBoard::getSize() const {
-  return _inside_limits;
-}
+const Dimensions& GameBoard::getSize() const {return _inside_limits;}
