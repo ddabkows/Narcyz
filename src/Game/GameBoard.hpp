02d0ff2d87d71e4694588418a8d2c899e0106e3d
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 14:28:00 +0200

    GameBoard headerfile

---
+++ src/Game/GameBoard.hpp
@@ -0,0 +1,31 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+*/
+
+
+#ifndef _GAME_BOARD_H_
+#define _GAME_BOARD_H_
+
+
+class GameBoard {
+  private:
+    // Trait
+    using myClass = GameBoard;
+    
+  public:
+    // Constructor
+    GameBoard() {}
+
+    // Copy
+    GameBoard(const myClass&) = delete;
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    GameBoard(myClass&&) = delete;
+    myClass& operator=(myClass&&) = delete;
+
+    // Destructor
+    ~GameBoard() = default;
+};
+#endif
