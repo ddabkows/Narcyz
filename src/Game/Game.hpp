559d169b002021e07601c380dce54c5160fcd96d
Dominik Dabkowski <dh.dabkowski@gmail.com>
2021-04-12 15:28:02 +0200

    Added Game header file

---
+++ src/Game/Game.hpp
@@ -0,0 +1,46 @@
+/*
+  Author : Dominik Dabkowski
+  Specs  : GNU C++ version 10.2 - "gg+ -std=c++17"
+  Code   : Class of Game
+*/
+
+
+#include "GameBoard.hpp"
+#include "GameEntity.hpp"
+
+
+#ifndef _GAME_H_
+#define _GAME_H_
+
+class Game final {
+  private:
+    //Trait
+    using myClass = Game;
+    GameBoard _gameboard;
+    GameEntity _player;
+
+  public:
+    // Constructor
+    Game() : _gameboard(800, 600),
+             _player(20, 50, 0, 800-50) {
+      
+    }
+
+    // Copy
+    Game(const myClass&) = delete;
+    myClass& operator=(const myClass&) = delete;
+
+    // Move
+    Game(myClass&&) = delete;
+    myClass& operator=(myClass&&) = delete;
+
+    // Methods
+
+    // Setters
+
+    // Getters
+
+    // Destructor
+    ~Game() = default;
+};
+#endif
