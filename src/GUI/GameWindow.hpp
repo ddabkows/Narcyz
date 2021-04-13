d6f8eabcadac81c5edef49620befe665b4c67bdf
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-04-13 21:58:35 +0200

    Added first image for player + changed background

--- src/GUI/GameWindow.hpp
+++ src/GUI/GameWindow.hpp
@@ -30,7 +30,7 @@ class GameWindow {
   public:
     // Constructor
     GameWindow(float player_size_x, float player_size_y, float player_pos_x, float player_pos_y) : 
-      _background(800.f, 600.f, 0.f, 0.f, sf::Color(0, 0, 0, 255), sf::Color(0, 0, 0, 255), 0.f, 0.f),
+      _background(800.f, 600.f, 0.f, 0.f, sf::Color::Green, sf::Color(0, 0, 0, 255), 0.f, 0.f),
       _player(player_size_x, player_size_y, player_pos_x, player_pos_y, sf::Color(255, 255, 255, 255), sf::Color(255, 255, 255, 255), 0.f, 0.f) {}
 
     // Copy
