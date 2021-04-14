af8024e2491f70f4ece4a8292bfdda76f9ed9b5d
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-04-14 04:00:20 +0200

    Move speed constant

--- src/Game/Game.hpp
+++ src/Game/Game.hpp
@@ -19,9 +19,9 @@ class Game final {
 
     const float _GAMEBOARD_SIZE_X = 800;
     const float _GAMEBOARD_SIZE_Y = 600;
-    const float _PLAYER_SIZE_X = 10;
+    const float _PLAYER_SIZE_X = 30;
     const float _PLAYER_SIZE_Y = _PLAYER_SIZE_X * 3.f;
-    const float _MOVE_DISTANCE = 50.f / _PLAYER_SIZE_X;
+    const float _MOVE_DISTANCE = 5.f;
     float _game_move_clock = 0;
     float _moment_of_jump = 0;
     bool _player_can_jump = true;
