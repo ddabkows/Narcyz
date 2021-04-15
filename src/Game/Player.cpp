d02ee7c29b549fa012dd544564108f219ac16ebc
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-04-15 03:40:52 +0200

    Added player methods

--- src/Game/Player.cpp
+++ src/Game/Player.cpp
@@ -14,8 +14,17 @@ void Player::lookLeft() {
   _looking_right = false;
 }
 
+void Player::lookRight() {
+  _looking_right = true;
+  _looking_left = false;
+}
+
+void Player::shoot(std::vector<Bullet> bullets) {
+  _weapon.shoot(bullets, _looking_right);
+}
 
 // Setters
 
 
 // Getters
+const float& Player::getMoveDistance() const {return _MOVE_DISTANCE;}
