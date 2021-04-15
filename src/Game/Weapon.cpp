021f04efe4faa789ec0cd6b76b5f92e116246a54
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-04-15 03:40:03 +0200

    Added shoot method

--- src/Game/Weapon.cpp
+++ src/Game/Weapon.cpp
@@ -9,6 +9,11 @@
 
 
 // Methods
+void Weapon::shoot(std::vector<Bullet> bullets, bool looking_right) {
+  float position_to_set = _position.x;
+  if (looking_right) {position_to_set += _size.x;}
+  bullets.emplace_back(3.f, 2.f, position_to_set, _position.y, _bullet_speed, looking_right);
+}
 
 
 // Getters
