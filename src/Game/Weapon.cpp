2f7e4cfc73619b2b59ed39c883dea3e6bc621700
Dominik Dabkowski <dominik.dabkowski@ulb.be>
2021-04-15 21:41:10 +0200

    Added projectile direction

--- src/Game/Weapon.cpp
+++ src/Game/Weapon.cpp
@@ -11,8 +11,10 @@
 // Methods
 void Weapon::shoot(std::vector<Bullet> bullets, bool looking_right) {
   float position_to_set = _position.x;
-  if (looking_right) {position_to_set += _size.x;}
-  bullets.emplace_back(3.f, 2.f, position_to_set, _position.y, _bullet_speed, looking_right);
+  float direction = 1.f;
+  if (!looking_right) {direction = -direction;}
+  else {position_to_set += _size.x;}
+  bullets.emplace_back(3.f, 2.f, position_to_set, _position.y, direction * _bullet_speed, looking_right);
 }
 
 
